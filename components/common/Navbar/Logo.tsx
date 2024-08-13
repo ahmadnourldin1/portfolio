3bf557357e33b90f9af7fb59fc380c2a42d15c13
ahmadnourldin1 <ahmadnourldin63@gmail.com>
2024-08-13 18:31:57 +0300

    feat: complete the logo plus fix the className Logic in the icon.

--- components/common/Navbar/Logo.tsx
+++ components/common/Navbar/Logo.tsx
@@ -4,8 +4,9 @@ import LogoIcon from '@/components/icons/LogoIcon'
 
 const Logo = () => {
   return (
-    <div>
-      <SVG icon={<LogoIcon/>}/>
+    <div  className="flex items-center gap-2">
+      <SVG icon={<LogoIcon className='!fill-white w-full h-full' />} className="w-5 h-5"/>
+      <span className='font-bold tracking-wide capitalize'>ahmad</span>
     </div>
   )
 }
