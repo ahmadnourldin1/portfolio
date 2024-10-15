b698d2bb94d58bde786aa51daf96c5d99e76a5b5
devsalemgrp <Dev@salemgrp.com>
2024-10-15 14:11:19 +0300

    feat:change photo.

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import ProfilePicture from "../public/body-photo.png";
 import CircleGroup from "@/components/icons/CircleGroup";
 import QuotesIcon from "@/components/icons/QuotesIcon";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -12,10 +13,16 @@ export default function Home() {
       <div className="w-full px-5 md:px-0 flex items-center justify-between flex-col md:flex-row">
         <div className="w-full md:w-1/2 flex items-start justify-center flex-col gap-5">
           <h1 className="text-3xl">
-            Elias is a <span className="text-primary">web designer</span> and <span className="text-primary">front-end developer</span>
+            Ahmad is a <span className="text-primary">full-stack web developer</span>
           </h1>
           <p className="text-grey text-sm md:w-[80%]">He crafts responsive websites where technologies meet creativity</p>
-          <PrimaryButton className="bg-backgroundSecondary border-primary border-2 rounded-none hover:bg-primary20 hover:text-white transition-all" text="Contact me!!" />
+          {/* Link the Button with onClick into my linkedin  */}
+          <Link href={`${process.env.NEXT_PUBLIC_LINKEDIN_URL}`} target="_blank">
+            <PrimaryButton
+              className="bg-backgroundSecondary border-primary border-2 rounded-none hover:bg-primary20 hover:text-white transition-all"
+              text="Contact me!!"
+            />
+          </Link>
         </div>
         <div className="w-full md:w-1/2 relative flex justify-center items-center flex-col">
           <SVG icon={<CrossCubes className="w-full h-full fill-primary" />} className="w-40 h-40 absolute left-20 top-10 -z-10" />
@@ -30,9 +37,9 @@ export default function Home() {
       <div className="w-full flex justify-center items-center my-16">
         <div className="flex justify-center items-end flex-col">
           <div className="relative p-5 border-grey border-[1px]">
-          <div className="w-7 h-5 flex justify-center items-center absolute -top-3 left-4 bg-background rounded-full">
+            <div className="w-7 h-5 flex justify-center items-center absolute -top-3 left-4 bg-background rounded-full">
               <SVG icon={<QuotesIcon className="w-4 h-4" />} className="absolute w-full h-full bg-backgroundSecondary rounded-full" />
-            </div>            
+            </div>
             <p>With great power comes great electricity bill</p>
             <div className="w-7 h-5 flex justify-center items-center absolute -bottom-3 right-4 bg-background rounded-full">
               <SVG icon={<QuotesIcon className="w-4 h-4" />} className="absolute w-full h-full bg-backgroundSecondary rounded-full" />
