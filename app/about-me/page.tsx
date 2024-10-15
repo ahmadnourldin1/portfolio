5cd7631142ba0d6be2f4c617a0049ab5781f4b92
ahmadnourldin1 <ahmadnourldin63@gmail.com>
2024-10-15 14:20:15 +0300

    feat:my name and location changed in the about section and name and email in the contact section.

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -10,8 +10,8 @@ const AboutMe = () => {
       </div>
       <div className="w-full flex flex-col md:flex-row justify-between items-center">
         <div className="md:w-1/2 w-full flex flex-col md:gap-5 gap-2">
-          <p className="text-grey text-sm md:text-base">Hello, i&apos;m Elias!</p>
-          <p className="text-grey text-sm md:text-base">I&apos;m a self-taught front-end developer based in Kyiv, Ukraine. I can develop responsive websites from scratch and raise them into modern user-friendly web experiences. </p>
+          <p className="text-grey text-sm md:text-base">Hello, i&apos;m Ahmad!</p>
+          <p className="text-grey text-sm md:text-base">I&apos;m a self-taught front-end developer based in Lebanon. I can develop responsive websites from scratch and raise them into modern user-friendly web experiences. </p>
           <p className="text-grey text-sm md:text-base">Transforming my creativity and knowledge into a websites has been my passion for over a year. I have been helping various clients to establish their presence online. I always strive to learn about the newest technologies and frameworks.</p>
         </div>
         <div className="w-1/2">
